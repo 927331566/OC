2b2cb3011a4aa42c636ebb67d4fd4303274c6f82
tingtingo <9273315663@qq.com>
2022-05-29 16:29:19 +0800

    nuonuo Commit

---
+++ nuonuo/nuonuo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nuonuo
+//
+//  Created by 王璐 on 2022/5/29.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
